e3316ca38ede45a067e628da97e9d8674b8e33fa
Katia Fernandes dos Anjos <kfanjos86@gmail.com>
2021-05-28 11:57:24 -0300

    Create Trabalho_pratico.py

---
+++ Trabalho_pratico.py
@@ -0,0 +1,12 @@
+nome = 'Roberto'
+idade = 35
+altura = 1.88
+cidades_visitadas = ['Paris', 'Barcelona', 'Chicago', 'Berlim']
+paises_visitados = ('França', 'Espanha', 'Estados Unidos', 'Alemanha')
+notas_cidades = {'Paris':5,
+ 'Barcelona':4,
+ 'Chicago':5,
+ 'Berlim':3}
+dinheiro_gasto = {1.000}
+dinheiro_guardado = 'R$ 500,00'
+proximas_viagens = ['Moscou', 'Londres', 'Lisboa]
